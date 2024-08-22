20334bfce95db1e7381555c81f6da6aeb3ab270a
imtahirabatool <btahira183@gmail.com>
2024-08-22 23:53:32 +0500

    Refactor

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <div>
     <Head>
-      <title>AI Rate My Professor</title>
+      <title>Profitics</title>
     </Head>
 
     <main className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-3xl font-bold mb-10">AI Rate My Professor</h1>
+      <h1 className="text-3xl font-bold mb-10">Profitics</h1>
       <ChatAssistant/>
     </main>
   </div>
